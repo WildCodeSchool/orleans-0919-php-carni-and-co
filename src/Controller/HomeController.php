44c71451b7071b3de04a1b4ab96484308988b0f2
Olivier-Jamet <olivierxavier.jamet@gmail.com>
2019-12-05 15:45:01 +0100

    add photos and text

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home_index")
+     * @Route("/", name="home_index")
      */
     public function index() :Response
     {
