91dfedfd9a11b33601b9caacd153f1cc69670cf4
Dimitri Gille <dimitri45220@hotmail.com>
2019-12-16 21:59:41 +0100

    mise en place de l'ajout d'aliment

---
+++ src/Entity/Food.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FoodRepository")
+ * @UniqueEntity("type")
+ */
+class Food
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     * @Assert\Length(
+     *     max = 80,
+     *     maxMessage = "Le type d'aliment ne doit pas excéder {{ limit }} caractères.")
+     * @Assert\NotBlank
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
