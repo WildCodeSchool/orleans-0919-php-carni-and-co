3f6b24c4d3ad4b9f82c149d1b051908130d1b4a6
Olivier-Jamet <olivierxavier.jamet@gmail.com>
2019-12-17 13:10:12 +0100

    Add label

--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AnimalRepository")
@@ -18,16 +19,19 @@ class Animal
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $name;
 
     /**
      * @ORM\Column(type="text")
+     ** @Assert\NotBlank()
      */
     private $description;
 
     /**
      * @ORM\Column(type="string", length=255)
+     ** @Assert\NotBlank()
      */
     private $image;
 
--- src/Form/AnimalType.php
+++ src/Form/AnimalType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Animal;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,15 @@ class AnimalType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('image')
+            ->add('name', TextType:: class, [
+                'label' => 'Titre'
+            ])
+            ->add('description', TextareaType:: class, [
+                'label' => 'Description'
+            ])
+            ->add('image', TextType:: class, [
+                'label' => 'Image'
+            ])
         ;
     }
 
