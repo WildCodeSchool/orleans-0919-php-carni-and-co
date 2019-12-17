6f80c58ac41064d0c69b5df0f8fc184f42c1a7df
Valentin Fauchère <valentin.fauchere@gmail.com>
2019-12-17 17:19:19 +0100

    Corrections routes et pages

--- src/Repository/NutrientRepository.php
+++ src/Repository/NutrientRepository.php
@@ -18,33 +18,4 @@ class NutrientRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Nutrient::class);
     }
-
-    // /**
-    //  * @return Nutrient[] Returns an array of Nutrient objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('n')
-            ->andWhere('n.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('n.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Nutrient
-    {
-        return $this->createQueryBuilder('n')
-            ->andWhere('n.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
