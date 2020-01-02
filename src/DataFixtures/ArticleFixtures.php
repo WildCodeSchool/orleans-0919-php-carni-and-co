d322eaf7a99af89b56df5cdb01db6a352bb9db95
Emilien De Macedo <emilien45100@gmail.com>
2020-01-02 13:34:10 +0100

    Fixtures Article

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,8 +3,28 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
 
-class ArticleFixtures
+class ArticleFixtures extends Fixture
 {
+    public function load(ObjectManager $manager)
+    {
 
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 1; $i <= 15; $i++) {
+            $article = new Article();
+            $article->setTitle($faker->sentence);
+            $article->setImage($faker->imageUrl());
+            $article->setDescription($faker->sentence);
+            $article->setDate($faker->dateTime);
+
+            $manager->persist($article);
+            $this->addReference('article_' . $i, $article);
+        }
+        $manager->flush();
+    }
 }
