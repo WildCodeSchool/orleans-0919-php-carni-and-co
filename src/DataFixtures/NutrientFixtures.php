9ff90a73eab9d5e06b8ec9854980f4051156b8bf
Valentin Fauchère <valentin.fauchere@gmail.com>
2020-01-06 15:44:25 +0100

    correction fixtures nutriments

--- src/DataFixtures/NutrientFixtures.php
+++ src/DataFixtures/NutrientFixtures.php
@@ -10,99 +10,29 @@ use Faker;
 
 class NutrientFixtures extends Fixture
 {
+    const NUTRIENTS = [
+        'Les protéines',
+        'Les glucides',
+        'Les graisses',
+        'Oméga-6',
+        'Oméga-3',
+        'Oméga-9',
+        'Huile de coco',
+        'Les vitamines',
+        'Les minéraux',
+        'L\'eau'
+    ];
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
-        $nameProtein = 'Les protéines';
+        foreach (self::NUTRIENTS as $nutrientName) {
+            $nutrientFixtures = new Nutrient();
+            $nutrientFixtures->setName($nutrientName);
+            $nutrientFixtures->setDescription($faker->paragraphs(4, true));
+            $nutrientFixtures->setImage($faker->imageUrl());
 
-        $proteinFixtures = new Nutrient();
-        $proteinFixtures->setName($nameProtein);
-        $proteinFixtures->setDescription($faker->paragraphs($nbSentences = 6, $variableNbSentences = true));
-        $proteinFixtures->setImage($faker->imageUrl());
-
-        $manager->persist($proteinFixtures);
-
-        $nameCarbo = 'Les glucides';
-
-        $carboHydrateFixtures = new Nutrient();
-        $carboHydrateFixtures->setName($nameCarbo);
-        $carboHydrateFixtures->setDescription($faker->paragraphs($nbSentences = 6, $variableNbSentences = true));
-        $carboHydrateFixtures->setImage($faker->imageUrl());
-
-        $manager->persist($carboHydrateFixtures);
-
-
-        $nameFat = 'Les graisses';
-
-        $fatFixtures = new Nutrient();
-        $fatFixtures->setName($nameFat);
-        $fatFixtures->setDescription($faker->paragraphs($nbSentences = 6, $variableNbSentences = true));
-        $fatFixtures->setImage($faker->imageUrl());
-
-        $manager->persist($fatFixtures);
-
-        $nameOmega6 = 'Oméga-6';
-
-        $omega6Fixtures = new Nutrient();
-        $omega6Fixtures->setName($nameOmega6);
-        $omega6Fixtures->setDescription($faker->paragraphs($nbSentences = 6, $variableNbSentences = true));
-        $omega6Fixtures->setImage($faker->imageUrl());
-
-        $manager->persist($omega6Fixtures);
-
-        $nameOmega3 = 'Oméga-3';
-
-        $omega3Fixtures = new Nutrient();
-        $omega3Fixtures->setName($nameOmega3);
-        $omega3Fixtures->setDescription($faker->paragraphs($nbSentences = 6, $variableNbSentences = true));
-        $omega3Fixtures->setImage($faker->imageUrl());
-
-        $manager->persist($omega3Fixtures);
-
-        $nameOmega9 = 'Oméga-9';
-
-        $omega9Fixtures = new Nutrient();
-        $omega9Fixtures->setName($nameOmega9);
-        $omega9Fixtures->setDescription($faker->paragraphs($nbSentences = 6, $variableNbSentences = true));
-        $omega9Fixtures->setImage($faker->imageUrl());
-
-        $manager->persist($omega9Fixtures);
-
-        $nameCocoNut = 'Huile de coco';
-
-        $cocoNutOilFixtures = new Nutrient();
-        $cocoNutOilFixtures->setName($nameCocoNut);
-        $cocoNutOilFixtures->setDescription($faker->paragraphs($nbSentences = 6, $variableNbSentences = true));
-        $cocoNutOilFixtures->setImage($faker->imageUrl());
-
-        $manager->persist($cocoNutOilFixtures);
-
-        $nameVitamin = 'Les vitamines';
-
-        $vitaminFixtures = new Nutrient();
-        $vitaminFixtures->setName($nameVitamin);
-        $vitaminFixtures->setDescription($faker->paragraphs($nbSentences = 6, $variableNbSentences = true));
-        $vitaminFixtures->setImage($faker->imageUrl());
-
-        $manager->persist($vitaminFixtures);
-
-        $nameMineral = 'Les minéraux';
-
-        $mineralFixtures = new Nutrient();
-        $mineralFixtures->setName($nameMineral);
-        $mineralFixtures->setDescription($faker->paragraphs($nbSentences = 6, $variableNbSentences = true));
-        $mineralFixtures->setImage($faker->imageUrl());
-
-        $manager->persist($mineralFixtures);
-
-        $nameWater = 'L\'eau';
-
-        $waterFixtures = new Nutrient();
-        $waterFixtures->setName($nameWater);
-        $waterFixtures->setDescription($faker->paragraphs($nbSentences = 6, $variableNbSentences = true));
-        $waterFixtures->setImage($faker->imageUrl());
-
-        $manager->persist($waterFixtures);
+            $manager->persist($nutrientFixtures);
+        }
 
         $manager->flush();
     }
