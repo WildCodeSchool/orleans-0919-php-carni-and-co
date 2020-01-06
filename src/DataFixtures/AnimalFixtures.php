29ea46ced71e06135801abfe99cc31c726965614
Olivier-Jamet <olivierxavier.jamet@gmail.com>
2020-01-06 10:08:06 +0100

    fixtures correction

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -5,23 +5,40 @@ namespace App\DataFixtures;
 use App\Entity\Animal;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Faker;
 
 class AnimalFixtures extends Fixture
 {
+    const ANIMALS = [
+        'Chien' => [
+            'name'=> 'Chien',
+            'description' => 'Le Chien (Canis lupus familiaris) est la sous-espèce domestique de Canis lupus, un 
+                              mammifère de la famille des Canidés (Canidae). C\'est un carnivore dit "opportuniste", il 
+                              consomme essentiellement de la viande mais aussi quelques fruits et légumes si 
+                              l\'occasion se présente. Il est également charognard.
+                               7.3 millions de chiens en France en 2018.',
+            'image' => 'chien.png',
+        ],
+        'Chat' => [
+            'name'=> 'Chat',
+            'description' => 'Le Chien (Canis lupus familiaris) est la sous-espèce domestique de Canis lupus, un 
+                              mammifère de la famille des Canidés (Canidae). C\'est un carnivore dit "opportuniste", 
+                              il consomme essentiellement de la viande mais aussi quelques fruits et légumes si 
+                              l\'occasion se présente. Il est également charognard.
+                              7.3 millions de chiens en France en 2018.',
+            'image' => 'chat.png',
+        ],
+    ];
+
     public function load(ObjectManager $manager)
     {
 
-        $faker = Faker\Factory::create('fr_FR');
-
-        for ( $i = 1 ; $i <= 2 ; $i++ ) {
-
+        foreach (self::ANIMALS as $key => $data) {
             $animal = new Animal();
-            $animal->setName($faker->word);
-            $animal->setDescription($faker->sentence);
-            $animal->setImage($faker->imageUrl());
+            $animal-> setName($key);
+            $animal-> setDescription($data['description']);
+            $animal-> setImage($data['image']);
 
-            $manager->persist($animal);
+            $manager -> persist($animal);
         }
         $manager->flush();
     }
