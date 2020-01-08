556a3959b5cdc42b9ec64a04de919376ddd7dbf6
Valentin Fauchère <valentin.fauchere@gmail.com>
2020-01-08 10:33:46 +0100

    début style partie générale nutrition

---
+++ src/Controller/NutrientVisitorController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Nutrient;
+use App\Form\NutrientType;
+use App\Repository\NutrientRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/nutrition")
+ */
+class NutrientVisitorController extends AbstractController
+{
+    /**
+     * @Route("/", name="nutrition_index", methods={"GET"})
+     */
+    public function indexVisitor(NutrientRepository $nutrientRepository): Response
+    {
+        return $this->render('nutrition/index.html.twig', [
+            'nutrients' => $nutrientRepository->findAll(),
+        ]);
+    }
+}
