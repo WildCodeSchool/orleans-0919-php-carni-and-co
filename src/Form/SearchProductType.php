b41c1eb584b802887552b94ce1876988061c83f5
Dimitri Gille <dimitri45220@hotmail.com>
2020-01-09 18:02:54 +0100

    mise en place de la barre de recherche dans les produits visiteurs 2

--- src/Form/SearchProductType.php
+++ src/Form/SearchProductType.php
@@ -1,14 +1,11 @@
 <?php
 
-
 namespace App\Form;
-
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 
 class SearchProductType extends AbstractType
 {
