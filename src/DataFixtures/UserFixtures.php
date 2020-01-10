60c3f4c12eea8162f9d59a3fa72ae2ff15cd948b
Olivier-Jamet <olivierxavier.jamet@gmail.com>
2020-01-10 04:35:37 +0100

    add inscription user

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        for ( $i = 1 ; $i <= 5 ; $i++ ) {
+            // Création d’un utilisateur de type “auteur”
+            $subscriber = new User();
+            $subscriber-> setUsername('username');
+            //$subscriber-> setEmail('email');
+            $subscriber-> setRoles(['ROLE_SUBSCRIBER']);
+            $subscriber-> setPassword($this->passwordEncoder->encodePassword($subscriber, 'subscriberpassword'));
+            $manager->persist($subscriber);
+            //Création d’un utilisateur de type “administrateur”
+            $admin = new User();
+            $admin->setUsername('username');
+            //$admin->setEmail('email');
+            $admin->setRoles(['ROLE_ADMIN']);
+            $admin->setPassword($this->passwordEncoder->encodePassword($admin,'adminpassword'));
+            $manager->persist($admin);
+        }
+        $manager->flush();
+    }
+}
