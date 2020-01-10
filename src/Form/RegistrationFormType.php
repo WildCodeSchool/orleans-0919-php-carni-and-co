18551cb45a83ab86acd726ca94c6def2419e31a2
Olivier <55036118+Olivier-Jamet@users.noreply.github.com>
2020-01-10 17:47:13 +0100

    Update RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -36,7 +36,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Votre mot de passse doit contenir au moins {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passse doit contenir au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
