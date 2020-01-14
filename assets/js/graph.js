943f48215070be99c3a3d4f5646d39df93c14030
Romain PETIT <rom.petit@live.fr>
2020-01-14 17:53:49 +0100

    fix travis bug

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -1,9 +1,9 @@
-let myColor = ['#39ca74', '#e54d42', '#d68910', '#3999d8', '#808080' ,'#800080'];
-let myLabel = ['Protéines', 'Lipides', 'Cendres', 'Fibres', 'Humidités', 'Glucides'];
+const myColor = ['#39ca74', '#e54d42', '#d68910', '#3999d8', '#808080', '#800080'];
+const myLabel = ['Protéines', 'Lipides', 'Cendres', 'Fibres', 'Humidités', 'Glucides'];
 
-let bringElement = document.getElementById('graph-bring');
+const bringElement = document.getElementById('graph-bring');
 
-let bring = bringElement.dataset.graph.split(",");
+let bring = bringElement.dataset.graph.split(',');
 bring = bring.map((coord) => {
     return +coord;
 });
@@ -11,23 +11,26 @@ bring = bring.map((coord) => {
 function getTotal() {
     let myTotal = 0;
     for (let j = 0; j < bring.length; j++) {
-        myTotal += (typeof bring[j] == 'number') ? bring[j] : 0;
+        myTotal += (typeof bring[j] === 'number') ? bring[j] : 0;
     }
     return myTotal;
 }
 
-function plotData() {
-    let canvas;
-    let ctx;
-    let lastend = 0;
-    let myTotal = getTotal();
-    let doc;
-    canvas = document.getElementById('canvas');
-    let x = (canvas.width) / 2;
-    let y = (canvas.height) / 2;
-    let r = 150;
+// Find that magical point
+function getPoint(c1, c2, radius, angle) {
+    return [c1 + Math.cos(angle) * radius, c2 + Math.sin(angle) * radius];
+}
 
-    ctx = canvas.getContext('2d');
+
+function plotData() {
+    let lastend = 0;
+    const myTotal = getTotal();
+    const canvas = document.getElementById('canvas');
+    const x = (canvas.width) / 2;
+    const y = (canvas.height) / 2;
+    const r = 150;
+
+    const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     for (let i = 0; i < bring.length; i++) {
@@ -42,10 +45,9 @@ function plotData() {
         ctx.beginPath();
         let start = [];
         let end = [];
-        let last = 0;
         let flip = 0;
         let textOffset = 0;
-        let percentage = (bring[i] / myTotal) * 100;
+        const percentage = (bring[i] / myTotal) * 100;
         start = getPoint(x, y, r - 20, (lastend + (Math.PI * 2 * (bring[i] / myTotal)) / 2));
         end = getPoint(x, y, r + 20, (lastend + (Math.PI * 2 * (bring[i] / myTotal)) / 2));
         if (start[0] <= x) {
@@ -63,16 +65,10 @@ function plotData() {
         ctx.stroke();
         // The labels
         ctx.font = '17px Arial';
-        ctx.fillText(myLabel[i] + ' ' + percentage.toFixed(2) + '%', end[0] + textOffset, end[1] - 4);
+        ctx.fillText(`${myLabel[i]} ${percentage.toFixed(2)}%`, end[0] + textOffset, end[1] - 4);
         // Increment Loop
         lastend += Math.PI * 2 * (bring[i] / myTotal);
-
     }
-}
-
-// Find that magical point
-function getPoint(c1, c2, radius, angle) {
-    return [c1 + Math.cos(angle) * radius, c2 + Math.sin(angle) * radius];
 }
 
 // The drawing
