396c78614d39fe2ddea4e13fe6b9bcc927a59f9e
Olivier <55036118+Olivier-Jamet@users.noreply.github.com>
2020-01-14 17:52:33 +0100

    Update RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -27,8 +27,6 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
