bf6b8efe4e76412f3b3cefc938431c57329edf33
Olivier <55036118+Olivier-Jamet@users.noreply.github.com>
2020-01-14 09:49:00 +0100

    Update UserFixtures.php

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -19,17 +19,13 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         for ($i = 1; $i <= 5; $i++) {
-            // Création d’un utilisateur de type “auteur”
             $subscriber = new User();
             $subscriber-> setUsername('username');
-            //$subscriber-> setEmail('email');
             $subscriber-> setRoles(['ROLE_SUBSCRIBER']);
             $subscriber-> setPassword($this->passwordEncoder->encodePassword($subscriber, 'subscriberpassword'));
             $manager->persist($subscriber);
-            //Création d’un utilisateur de type “administrateur”
             $admin = new User();
             $admin->setUsername('username');
-            //$admin->setEmail('email');
             $admin->setRoles(['ROLE_ADMIN']);
             $admin->setPassword($this->passwordEncoder->encodePassword($admin, 'adminpassword'));
             $manager->persist($admin);
