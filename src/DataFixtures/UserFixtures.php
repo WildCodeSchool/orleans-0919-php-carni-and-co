f5e1a298a0eaf78ef1479fc44b61374405b22d18
Olivier-Jamet <olivierxavier.jamet@gmail.com>
2020-01-15 11:01:08 +0100

    fix user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,10 +6,12 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Faker;
 
 class UserFixtures extends Fixture
 {
     private $passwordEncoder;
+
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
@@ -18,18 +20,31 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+
+        $faker = Faker\Factory::create('fr_FR');
+
         for ($i = 1; $i <= 5; $i++) {
             $subscriber = new User();
-            $subscriber-> setUsername('username');
+            $subscriber-> setUsername($faker->firstName);
+            $subscriber-> setEmail($faker->email);
             $subscriber-> setRoles(['ROLE_SUBSCRIBER']);
             $subscriber-> setPassword($this->passwordEncoder->encodePassword($subscriber, 'subscriberpassword'));
             $manager->persist($subscriber);
+
+        }
+        $subscriber = new User();
+        $subscriber-> setUsername('username');
+        $subscriber-> setEmail('username@monsite.com');
+            $subscriber-> setRoles(['ROLE_SUBSCRIBER']);
+            $subscriber-> setPassword($this->passwordEncoder->encodePassword($subscriber, 'subscriberpassword'));
+            $manager->persist($subscriber);
+
             $admin = new User();
-            $admin->setUsername('username');
+            $admin->setUsername('admin');
+            $admin->setEmail('admin@monsite.com');
             $admin->setRoles(['ROLE_ADMIN']);
             $admin->setPassword($this->passwordEncoder->encodePassword($admin, 'adminpassword'));
             $manager->persist($admin);
-        }
         $manager->flush();
     }
 }
