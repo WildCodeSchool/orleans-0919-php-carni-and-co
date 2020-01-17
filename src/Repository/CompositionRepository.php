933ae5b3cd8f147ecec415bdb5d08865b91e8694
Dimitri Gille <dimitri45220@hotmail.com>
2020-01-17 17:41:22 +0100

    retrait du code commenté

--- src/Repository/CompositionRepository.php
+++ src/Repository/CompositionRepository.php
@@ -18,33 +18,4 @@ class CompositionRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Composition::class);
     }
-
-    // /**
-    //  * @return Composition[] Returns an array of Composition objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Composition
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
